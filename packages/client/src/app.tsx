8ef4b27c0df0840c660f77792400f0520f8c191c
Ilya <ilya.shutskiy@ifellow.ru>
2021-12-30 17:38:05 +0300

    feat(client): init client

---
+++ packages/client/src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">client</div>
+  );
+}
+
+export default App;
---
+++ packages/client/src/store/features/counter/counterSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export interface CounterState {
+  value: number;
+  status: 'idle' | 'loading' | 'failed';
+}
+
+const initialState: CounterState = {
+  value: 0,
+  status: 'idle',
+};
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {},
+});
+
+export default counterSlice.reducer;
